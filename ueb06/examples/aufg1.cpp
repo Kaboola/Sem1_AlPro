2e755125516f2d9de4942220b597965bc02f91e5
Kaboola <155463810+Kaboola@users.noreply.github.com>
2024-01-14 13:31:15 +0100

    finished mergesort

--- ueb06/examples/aufg1.cpp
+++ ueb06/examples/aufg1.cpp
@@ -6,8 +6,10 @@
 
 int main() {
   misc::Zeitmesser zeitmesser;
-  std::vector<int> bsp_zahlen(100000);
+  std::vector<int> bsp_zahlen(1000);
   misc::fuelle_zufaellig(bsp_zahlen);
+  
+  std::cout << "Vor der Sortierung:" << std::endl;
   misc::drucke_array(bsp_zahlen);
 
   // Timer starten.
@@ -16,12 +18,13 @@ int main() {
   mergesort(bsp_zahlen);
 
   // Timer stoppen.
-  zeitmesser.stop();
 
+  zeitmesser.stop();
+  std::cout << "Nach der Sortierung:" << std::endl;
   misc::drucke_array(bsp_zahlen);
 
   // Gemessene Zeit in Milisekunden ausgeben
 std::cout << "Sortiert in:\n"  << zeitmesser.get_minutes() << " Min; " << zeitmesser.get_seconds() << " Sek; " << zeitmesser.get_milliseconds() << " Milli; " << zeitmesser.get_microseconds() << " Mikro;" << std::endl;
 }
 
-// g++ -std=c++17 -I./include \src/hilfsfunktionen.cpp src/mergesort.cpp \examples/aufg1.cpp -o build/aufg1
+// g++ -std=c++17 -I./include src/hilfsfunktionen.cpp src/mergesort.cpp examples/aufg1.cpp -o build/aufg1
--- ueb06/src/mergesort.cpp
+++ ueb06/src/mergesort.cpp
@@ -13,15 +13,13 @@ void mergesort(std::vector<int>& liste) {
         std::vector<int> array_rechts;
 
 // Setze die erste Hälfte der Elemente aus "liste" in "array_links"
-        for (int i = 0; i < halbiert; i++) {
-            array_links.push_back(liste.front());
-            liste.erase(liste.begin());
-        }
-
-// Setze die zweite Hälfte der Elemente aus "liste" in "array_rechts"
-        for (int i = halbiert; i < arraylaenge; i++) {
-            array_rechts.push_back(liste.front());
-            liste.erase(liste.begin());
+        for (int i = 0; i < arraylaenge; i++) {
+            if (i < halbiert) {
+                array_links.push_back(liste[i]);
+            }
+            else {
+                array_rechts.push_back(liste[i]);
+            }
         }
 
 // CONQUER-SCHRITT
@@ -38,5 +36,35 @@ void mergesort(std::vector<int>& liste) {
 }
 
 void merge(const std::vector<int>& links, const std::vector<int>& rechts, std::vector<int>& merged) {
+    int laenge_links = links.size();    
+    int laenge_rechts = rechts.size();
+    int i = 0;                          // Index für array_links
+    int j = 0;                          // Index für array_rechts
+    int k = 0;                          // Index für merged
 
+    while (i < laenge_links && j < laenge_rechts) {
+// Setze entweder Element aus "links" in merged[k] oder Element aus "rechts", abhängig der Größe
+        if (links[i] <= rechts[j]) {
+            merged[k] = links[i];
+            i++;
+        }
+        else {
+            merged[k] = rechts[j];
+            j++;
+        }
+        k++;
+    }
+// Wenn nicht mehr verglichen werden kann, ist entweder ein Element in "links" oder eines in "rechts" übrig.
+// Wir gehen abschließend durch beide Arrays und setzen das fehlende Element in merged[k], falls vorhanden.
+    while (i < laenge_links) {
+        merged[k] = links[i];
+        i++;
+        k++;
+    }
+
+    while (j < laenge_rechts) {
+        merged[k] = rechts[j];
+        j++;
+        k++;
+    }
 }
